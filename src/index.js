408e17b2db221a9e869c6649d632b0b6be494c20
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 14:32:39 +0300

    Rokas

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
